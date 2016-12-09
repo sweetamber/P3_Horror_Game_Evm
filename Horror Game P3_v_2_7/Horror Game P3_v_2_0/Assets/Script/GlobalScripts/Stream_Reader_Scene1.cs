4967e96e57f1e6de0c9d20d3dd88574281979bfc
sweetamber <rasmus_johannsen@hotmail.com>
2016-12-10 00:54:54 +0100

    Added tutorial playOnce, added adjusted player movement, added EVM second Level

---
+++ Horror Game P3_v_2_7/Horror Game P3_v_2_0/Assets/Script/GlobalScripts/Stream_Reader_Scene1.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+
+public class Stream_Reader_Scene1 : MonoBehaviour {
+
+    //Path for the file
+    public string filePath = @"C:\evm_data_pulse.txt";
+
+    GameObject Reader;
+    //Holds the string read from the file
+    string current;
+
+    //Runs the CoRoutine again once it is done, to ensure that it can only be run once
+    bool empty;
+    //Counts how many times a new string has been read
+    public int Counter;
+    //Parse current into an int
+    public float convert;
+
+    public float baseLine;
+    float baseLineTemp;
+
+    void Start()
+    {
+            baseLine = PlayerPrefs.GetFloat("baseline");
+            Counter = PlayerPrefs.GetInt("Counter");     
+    }
+
+    void OnDestroy()
+    {
+        PlayerPrefs.SetFloat("baseline", baseLine);
+        PlayerPrefs.SetInt("Counter", Counter);
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        if (empty == false)
+        {          
+            StartCoroutine("WaitAndPrint");
+            //Debug.Log("StreamReader Initialized");
+            //Debug.Log(empty + " Not Empty");
+            empty = true;
+        }
+    }
+
+
+    IEnumerator WaitAndPrint()
+    {
+        yield return new WaitForSeconds(1);
+
+        using (StreamReader sr = new StreamReader(filePath))
+        {          
+            do
+            {
+                current = sr.ReadLine();
+                convert = float.Parse(current);
+                //Debug.Log(current + " HearthRate");                
+
+                if (Counter <= 60)
+                {
+                    Debug.Log(Counter + " " + baseLineTemp + " HR BaseLine After 60");
+                    Counter++;
+                    if (Counter < 60)
+                    {
+                        
+                        baseLineTemp = baseLineTemp + convert;
+                    }
+                    if (Counter == 60)
+                    {
+                        baseLine = baseLineTemp / 60;
+                        Debug.Log("Baseline: " + baseLine);
+                    }
+                }           
+                empty = false;
+            } while (empty);
+        }
+    }
+}
+
--- Horror Game P3_v_2_7/Horror Game P3_v_2_0/Assets/Script/Scene1/Tutorial/Tutorial_Events.cs
+++ Horror Game P3_v_2_7/Horror Game P3_v_2_0/Assets/Script/Scene1/Tutorial/Tutorial_Events.cs
@@ -1,14 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Tutorial_Events : MonoBehaviour {
+public class Tutorial_Events : MonoBehaviour
+{
     //This script is attached to "Scene 1: tutorial Section -> Tutorial Events" && "Scene 1: tutorial Section -> panel_Switches -> cellDoorLight"
 
     Light tutorial_Light;
-        
+
     bool blinkOn = false;
     bool ready = true;
     public bool narrationOver = false;
+
+    //This to cancel narration a second time, if narration played once, then narrationPlayedOnce = 1, if = 1, then skip narratioon
+    int narrationPlayedOnce;
 
     public float timeBlink = 0.5f;
     public float timeTriggerEvents = 10f;
@@ -22,12 +26,16 @@ public class Tutorial_Events : MonoBehaviour {
         GameObject spotLight = GameObject.Find("cellDoorLight");
         tutorial_Light = spotLight.GetComponent<Light>();
         audio1 = GetComponent<AudioSource>();
+
+        //Takes the value from the PlayerPrefs and sets it to narration played once, if the narration has been played, then it will be set to one and therefor not played again
+        //If PlayerPrefs holds no record of the variable NarrationDoneOnce, then it is set to zero
+        narrationPlayedOnce = PlayerPrefs.GetInt("narrationDoneOnce", 0);
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        Debug.Log(narrationPlayedOnce);
         //made this if statement to ensure that the coroutine is only run once every second.. So that the light will appear to blink
         if (blinkOn == true)
         {
@@ -60,12 +68,24 @@ public class Tutorial_Events : MonoBehaviour {
         //Debug.Log("TriggerEvent");
 
         //ready indicates that the player has entered the trigger zone for the first time, and is made false, so that it wont play tutorial narration again
-        if (ready && narrationOver == false) {
+        if (ready == true && narrationOver == false && narrationPlayedOnce == 0)
+        {
             //Debug.Log(ready);
             audio1.Play();
+             narrationPlayedOnce = 1;
+            PlayerPrefs.SetInt("narrationDoneOnce", narrationPlayedOnce);
+            ready = false;
+        }
+        //when the script is run a second time during runTime, the value narrationPlayedOnce is = 1 and ready is again equal to true, thus this will be run instead of narration 
+        else if (ready == true && narrationPlayedOnce == 1)
+        {
+            Debug.Log("PLAYED!!!!!!!!");
+            timeTriggerEvents = 0;
             ready = false;
         }
         // event timing, counts to the amount of seconds that the narration takes, and then activates the blinking red button, and enables the player to press it though another script (tutorialDoor)
         StartCoroutine("EventTiming");
     }
+
+
 }
